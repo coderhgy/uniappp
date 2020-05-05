b681c6135566bdf87892b79932931c0280332d9f
hgy <coderhgy@users.noreply.github.com>
2020-05-05 17:12:44 +0800

    Initial commit

---
+++ pages/msg-list/msg-list.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="p-2">
+		<block v-for="(item,index) in list" :key="index">
+			<view class="mb-2 p-2 line-h d-flex flex-column rounded bg-white">
+				<view class="font-md text-dark">{{item.title}}</view>
+				<text class="line-h-md text-light-muted">{{item.desc}}</text>
+				<text class="line-h-md text-light-muted">{{item.time}}</text>
+			</view>
+		</block>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list:[
+					{
+						title:"uni-appDemo",
+						desc:"非常牛逼",
+						time:"5月28"
+					},
+					{
+						title:"uni-appDemo",
+						desc:"非常牛逼",
+						time:"5月28"
+					}
+				]
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+page{ background: #EEEEEE; }
+</style>
